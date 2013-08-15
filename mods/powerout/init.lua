ed34f1d080aa94cae0e1b22ab50d9ee32030a9b2
bas080 <bas080@hotmail.com>
2013-08-15 22:33:06 +0200

    Improved some textures and added coal plant to powerout mod

--- mods/powerout/init.lua
+++ mods/powerout/init.lua
@@ -29,7 +29,7 @@ minetest.register_node("powerout:light", {
     local meta = minetest.get_meta(pos)
     local minp = {x=pos.x-range, y=pos.y-range, z=pos.z-range}
     local maxp = {x=pos.x+range, y=pos.y+range, z=pos.z+range}
-    local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "powerout:solar_panel")
+    local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "group:power_source")
     local node = minetest.get_node(pos)
     for i in ipairs(source_in_area) do
       local meta = minetest.get_meta(source_in_area[i])
@@ -65,31 +65,32 @@ minetest.register_node("powerout:light_on", {
   sounds = default.node_sound_glass_defaults(),
   after_dig_node = function(pos, oldnode, oldmetadata, digger)
     local range = 20
-    local amount = 5
     local minp = {x=pos.x-range, y=pos.y-range, z=pos.z-range}
     local maxp = {x=pos.x+range, y=pos.y+range, z=pos.z+range}
-    local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "powerout:solar_panel")
+    local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "group:power_source")
     for i in ipairs(source_in_area) do
       local node = minetest.get_node(source_in_area[i])
       local meta = minetest.get_meta(source_in_area[i])
       local power =  meta:get_int("power")
+      local max =  meta:get_int("max_power")
       if power > 0 then
         meta:set_int("power", power - 1 )
-        meta:set_string("infotext", (power-1).."/5" )
+        meta:set_string("infotext", (power-1).."/"..max )
         return
       end
     end
   end,
 })
 
+local max = 5
 minetest.register_node("powerout:solar_panel", {
   description = "Solar panel",
   drawtype = "nodebox",
   paramtype = "light",
   paramtype2 = "facedir",
-  tiles = {"powerout_solar_panel.png", "powerout_solar_panel_side.png"},
+  tiles = {"powerout_solar_panel.png", "default_steel_block.png", "powerout_solar_panel_side.png"},
   light_source = 3,
-  groups = { oddly_breakable_by_hand=1 },
+  groups = { oddly_breakable_by_hand=1, power_source=1 },
   node_box = {
     type = "fixed",
     fixed = {
@@ -99,7 +100,7 @@ minetest.register_node("powerout:solar_panel", {
   on_construct = function(pos)
     local range = 20
     local meta = minetest.get_meta(pos)
-    meta:set_int("max_power", 5)
+    meta:set_int("max_power", max)
     meta:set_int("power", 0)
     local minp = {x=pos.x-range, y=pos.y-range, z=pos.z-range}
     local maxp = {x=pos.x+range, y=pos.y+range, z=pos.z+range}
@@ -108,8 +109,8 @@ minetest.register_node("powerout:solar_panel", {
       local node = minetest.get_node(lights_in_area[i])
       minetest.set_node(lights_in_area[i], {name="powerout:light_on", param2=node.param2})
       meta:set_int("power", i)
-      meta:set_string("infotext", i.."/"..5 )
-      if 5 <= i then return end
+      meta:set_string("infotext", i.."/"..max )
+      if max <= i then return end
     end
   end,
   on_destruct = function(pos)
@@ -125,7 +126,68 @@ minetest.register_node("powerout:solar_panel", {
       local meta = minetest.get_meta(pos)
       local minp = {x=lights_in_area[i].x-range, y=lights_in_area[i].y-range, z=lights_in_area[i].z-range}
       local maxp = {x=lights_in_area[i].x+range, y=lights_in_area[i].y+range, z=lights_in_area[i].z+range}
-      local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "powerout:solar_panel")
+      local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "group:power_source")
+      local node = minetest.get_node(lights_in_area[i])
+      for ii in ipairs(source_in_area) do
+        if vector.distance(pos, source_in_area[ii]) ~= 0  then
+          local meta = minetest.get_meta(source_in_area[ii])
+          local power = meta:get_int("power")
+          local max_power = meta:get_int("max_power")
+          if power < max_power then
+            meta:set_int("power", power+1 )
+            meta:set_string("infotext", (power+1).."/"..max_power )
+            minetest.set_node(lights_in_area[i], {name="powerout:light_on", param2=node.param2})
+            break
+          else
+            minetest.set_node(lights_in_area[i], {name="powerout:light", param2=node.param2})
+          end
+        else
+          minetest.set_node(lights_in_area[i], {name="powerout:light", param2=node.param2})
+        end
+      end
+    end
+  end,
+})
+
+local max = 10
+minetest.register_node("powerout:coal_plant", {
+  description = "Coal plant",
+  drawtype = "node",
+  paramtype = "light",
+  paramtype2 = "facedir",
+  tiles = {"default_cobble.png", "default_cobble.png", "default_cobble.png", "default_cobble.png", "default_cobble.png", "powerout_coal_plant.png"},
+  light_source = 3,
+  groups = { oddly_breakable_by_hand=1, power_source=1 },
+  on_construct = function(pos)
+    local range = 20
+    local meta = minetest.get_meta(pos)
+    meta:set_int("max_power", max)
+    meta:set_int("power", 0)
+    local minp = {x=pos.x-range, y=pos.y-range, z=pos.z-range}
+    local maxp = {x=pos.x+range, y=pos.y+range, z=pos.z+range}
+    local lights_in_area = minetest.env:find_nodes_in_area(minp, maxp, "powerout:light")
+    for i in ipairs(lights_in_area) do
+      local node = minetest.get_node(lights_in_area[i])
+      minetest.set_node(lights_in_area[i], {name="powerout:light_on", param2=node.param2})
+      meta:set_int("power", i)
+      meta:set_string("infotext", i.."/"..max )
+      if max <= i then return end
+    end
+  end,
+  on_destruct = function(pos)
+    local range = 20
+    local meta = minetest.get_meta(pos)
+    local amount = meta:get_int("power")
+    local minp = {x=pos.x-range, y=pos.y-range, z=pos.z-range}
+    local maxp = {x=pos.x+range, y=pos.y+range, z=pos.z+range}
+    local lights_in_area = minetest.env:find_nodes_in_area(minp, maxp, "powerout:light_on")
+    for i in ipairs(lights_in_area) do
+      if amount < i then return end
+      local range = 20
+      local meta = minetest.get_meta(pos)
+      local minp = {x=lights_in_area[i].x-range, y=lights_in_area[i].y-range, z=lights_in_area[i].z-range}
+      local maxp = {x=lights_in_area[i].x+range, y=lights_in_area[i].y+range, z=lights_in_area[i].z+range}
+      local source_in_area = minetest.env:find_nodes_in_area(minp, maxp, "group:power_source")
       local node = minetest.get_node(lights_in_area[i])
       for ii in ipairs(source_in_area) do
         if vector.distance(pos, source_in_area[ii]) ~= 0  then
@@ -156,7 +218,15 @@ minetest.register_craft({
   }
 })
 
---[[
+minetest.register_craft({
+  output = 'powerout:coal_plant 4',
+  recipe = {
+    {'default:cobble', 'default:cobble', 'default:cobble'},
+    {'default:cobble', 'default:coalblock', 'default:cobble'},
+    {'default:cobble', 'default:cobble', 'default:cobble'},
+  }
+})
+
 minetest.register_abm({
   nodenames = {"default:torch"},
   interval = 1,
@@ -165,4 +235,3 @@ minetest.register_abm({
     minetest.set_node(pos, {name="powerout:light", param2 = node.param2})
   end,
 })
-]]
