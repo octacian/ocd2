8a733b72bde4cbb2bf7a251033572b40464e886e
bas080 <bas080@hotmail.com>
2013-08-21 01:29:15 +0200

    Added new stone brick and added powerout lamp to creative inventory

--- mods/ocd/init.lua
+++ mods/ocd/init.lua
@@ -16,12 +16,29 @@ minetest.register_node("ocd:cobble_in_stone", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
+minetest.register_node("ocd:stone_brick", {
+	description = "Stone Brick",
+	tiles = {"ocd_stone_brick.png"},
+	is_ground_content = true,
+	groups = {cracky=3, stone=1},
+	drop = 'default:cobble',
+	sounds = default.node_sound_stone_defaults(),
+})
+
 minetest.register_craft({
 	output = 'ocd:cobble_in_stone 9',
 	recipe = {
 		{'default:stone', 'default:stone', 'default:stone'},
 		{'default:stone', 'default:cobble', 'default:stone'},
 		{'default:stone', 'default:stone', 'default:stone'},
+	}
+})
+
+minetest.register_craft({
+	output = 'ocd:stone_brick 4',
+	recipe = {
+		{'ocd:stone_in_cobble', 'ocd:stone_in_cobble'},
+		{'ocd:stone_in_cobble', 'ocd:stone_in_cobble'},
 	}
 })
 
