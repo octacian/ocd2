98a5f1754cb3f26191b28072653b91a79e0c5d67
octacian <enduffy2014@outlook.com>
2017-04-23 13:38:33 -0700

    Gamemode: Prevent damage and breath depletion in creative

--- mods/ocd/gamemode.lua
+++ mods/ocd/gamemode.lua
@@ -3,6 +3,10 @@
 local gamemodes = {}
 local creative  = minetest.setting_getbool("creative_mode")
 
+---
+--- Callbacks
+---
+
 -- [register] On join set gamemode
 minetest.register_on_joinplayer(function(player)
   ocd.set_gamemode(player, ocd.get_gamemode(player))
@@ -13,6 +17,16 @@ minetest.register_on_placenode(function(pos, newnode, player, oldnode, itemstack
   local mode = gamemodes[ocd.get_gamemode(player)]
   return mode.stack_unlimited
 end)
+
+-- [register] On hp change
+minetest.register_on_player_hpchange(function(player, hp_change)
+  local def = ocd.get_gamemode_def(ocd.get_gamemode(player))
+  if def.damage == false then
+    return 0
+  else
+    return hp_change
+  end
+end, true)
 
 ---
 --- API
@@ -113,6 +127,8 @@ ocd.register_gamemode("creative", {
   range = 10,
   stack_unlimited = true,
   item_drops = "auto",
+  damage = false,
+  breath = false,
   hand_capabilities = {
 		full_punch_interval = 0.5,
 		max_drop_level = 3,
